b82c369c09fd6096e3aafcd9af2dc0e1c1f5d905
Jari Bakken <jari.bakken@gmail.com>
2013-03-28 18:16:53 +0100

    Donwload the 64-bit version

--- Rakefile
+++ Rakefile
@@ -3,8 +3,6 @@ require 'selenium-webdriver'
 task :default do
   ENV['DISPLAY'] = ":99"
 
-  puts Dir['./**/*']
-
   Selenium::WebDriver::Chrome.driver_path = File.expand_path('chrome-linux/chromedriver')
   Selenium::WebDriver::Chrome.path = File.expand_path('chrome-linux/chrome')
   Selenium::WebDriver.for(:chrome).quit
--- before.sh
+++ before.sh
@@ -6,9 +6,9 @@ CHROME_REVISION=191150
 
 sh -e /etc/init.d/xvfb start
 sudo apt-get install -y unzip libxss1
-curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux/$CHROME_REVISION/chrome-linux.zip"
+curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/$CHROME_REVISION/chrome-linux.zip"
 unzip chrome-linux.zip
-curl -L "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux/$CHROME_REVISION/chrome-linux.test/chromedriver" > chrome-linux/chromedriver
+curl -L "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/$CHROME_REVISION/chrome-linux.test/chromedriver" > chrome-linux/chromedriver
 chmod +x chrome-linux/chromedriver
 
 file chrome-linux/chromedriver
