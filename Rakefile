b5dfad85d1ddbe99040cd55eaa64a17c8437cec4
Jari Bakken <jari.bakken@gmail.com>
2013-03-28 17:15:18 +0100

    Trying the chrome download

--- Rakefile
+++ Rakefile
@@ -1,10 +1,3 @@
-require 'rubygems'
-require 'selenium-webdriver'
-
 task :default do
-  ENV['DISPLAY'] = ":99"
-  ENV['PATH'] += ":#{File.expand_path('chrome-linux')}"
-
-  $DEBUG = true
-  Selenium::WebDriver.for(:chrome).quit
+  puts "ok"
 end
--- before.sh
+++ before.sh
@@ -1,3 +1,14 @@
 #/bin/sh
 
-env | sort
+set -x
+
+CHROME_REVISION=191150
+
+sudo apt-get install -y unzip libxss1
+curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux/$CHROME_REVISION/chrome-linux.zip"
+unzip chrome-linux.zip
+curl -L "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux/$CHROME_REVISION/chrome-linux.test/chromedriver" > chrome-linux/chromedriver
+chmod +x chrome-linux/chromedriver
+
+file chrome-linux/chromedriver
+ls -la chrome-linux
