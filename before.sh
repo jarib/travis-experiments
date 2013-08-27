37723b5f386fc09d6e0a0d0445b96d2927acf88b
Jari Bakken <jari.bakken@gmail.com>
2013-08-27 19:33:52 +0200

    Show versions

--- before.sh
+++ before.sh
@@ -1,16 +1,7 @@
 #/bin/sh
 
 set -x
+set -e
 
-CHROME_REVISION=191150
-
-type -a chrome
-
-sh -e /etc/init.d/xvfb start
-sudo apt-get install -y unzip libxss1
-curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/$CHROME_REVISION/chrome-linux.zip"
-unzip chrome-linux.zip
-curl -L "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/$CHROME_REVISION/chrome-linux.test/chromedriver" > chrome-linux/chromedriver
-chmod +x chrome-linux/chromedriver
-
-sudo chmod 1777 /dev/shm
+node --version
+npm --version
