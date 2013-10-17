07a871cb6137310a610a4d078b905ced36989701
Jari Bakken <jari.bakken@gmail.com>
2013-10-18 01:00:18 +0200

    Try sandbox setup - https://code.google.com/p/chromium/wiki/LinuxSUIDSandboxDevelopment

--- before.sh
+++ before.sh
@@ -9,17 +9,21 @@ CHROME_REVISION="228611"
 CHROMEDRIVER_VERSION="2.4"
 sh -e /etc/init.d/xvfb start
 
-ps auxww | grep xvfb
-echo "DISPLAY: #{DISPLAY}"
+sudo chmod 1777 /dev/shm
 
 sudo apt-get install -y unzip libxss1
 curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/${CHROME_REVISION}/chrome-linux.zip"
 unzip chrome-linux.zip
+
 curl -L -O "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
 unzip chromedriver_linux64.zip
+
+sudo chown root:root chrome-linux/chrome_sandbox
+sudo chmod 4755 chrome-linux/chrome_sandbox
+export CHROME_DEVEL_SANDBOX="$PWD/chrome-linux/chrome_sandbox"
+
 mv chromedriver chrome-linux/chromedriver
 chmod +x chrome-linux/chromedriver
-sudo chmod 1777 /dev/shm
 
 ls -la chrome-linux
 file chrome-linux/chromedriver
