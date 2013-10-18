73c6d8be463e59993cb0b8fe7e983fdb7fea4c3b
Jari Bakken <jari.bakken@gmail.com>
2013-10-18 17:25:18 +0200

    Checking /etc/hosts

--- Rakefile
+++ Rakefile
@@ -1,14 +1,5 @@
 require 'bundler/setup'
-require 'selenium-webdriver'
 
 task :default do
-  ENV['PATH'] = [ENV['PATH'], File.expand_path("../chrome-linux", __FILE__)].join(':')
-  ENV['DISPLAY'] = ":99"
-  
-  $DEBUG = true
-
-  d = Selenium::WebDriver.for :chrome, :args => %w[--no-sandbox]
-  d.get "http://google.com"
-  p d.title
-  d.quit
+  puts :ok
 end
--- before.sh
+++ before.sh
@@ -5,26 +5,28 @@ set -e
 
 env | sort
 
-export CHROME_REVISION="228611"
-export CHROMEDRIVER_VERSION="2.4"
-sh -e /etc/init.d/xvfb start
-export DISPLAY=:99
+cat /etc/hosts
 
-sudo chmod 1777 /dev/shm
-
-sudo apt-get install -y unzip libxss1
-curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/${CHROME_REVISION}/chrome-linux.zip"
-unzip chrome-linux.zip
-
-curl -L -O "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
-unzip chromedriver_linux64.zip
-
-sudo chown root:root chrome-linux/chrome_sandbox
-sudo chmod 4755 chrome-linux/chrome_sandbox
-export CHROME_DEVEL_SANDBOX="$PWD/chrome-linux/chrome_sandbox"
-
-mv chromedriver chrome-linux/chromedriver
-chmod +x chrome-linux/chromedriver
-
-ls -la chrome-linux
-cat /etc/fstab
+# export CHROME_REVISION="228611"
+# export CHROMEDRIVER_VERSION="2.4"
+# sh -e /etc/init.d/xvfb start
+# export DISPLAY=:99
+# 
+# sudo chmod 1777 /dev/shm
+# 
+# sudo apt-get install -y unzip libxss1
+# curl -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/${CHROME_REVISION}/chrome-linux.zip"
+# unzip chrome-linux.zip
+# 
+# curl -L -O "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
+# unzip chromedriver_linux64.zip
+# 
+# sudo chown root:root chrome-linux/chrome_sandbox
+# sudo chmod 4755 chrome-linux/chrome_sandbox
+# export CHROME_DEVEL_SANDBOX="$PWD/chrome-linux/chrome_sandbox"
+# 
+# mv chromedriver chrome-linux/chromedriver
+# chmod +x chrome-linux/chromedriver
+# 
+# ls -la chrome-linux
+# cat /etc/fstab
